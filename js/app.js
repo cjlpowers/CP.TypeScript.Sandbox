2f64d579343de8e149d5af9f7ba87bfa5b796f23
Chris Powers <cpowers@outlook.com>
2014-12-04 01:28:17 -0600

    Removing unnecessary functions

--- js/app.js
+++ js/app.js
@@ -109,80 +109,14 @@ var Sandbox;
                             _this.structureJson = JSON.stringify(definition, null, "\t");
                         }
                     });
-                    this.structureJson = JSON.stringify(this.getStructureDeinition(), null, "\t");
+                    this.structureJson = JSON.stringify(this.getStructureDefinition(), null, "\t");
                 }
                 Controller.prototype.loadStructure = function (structureDefinition) {
                     var structure = CP.Mechanical.Structure.load(structureDefinition);
                     structure.solve();
                     this.structure = structure;
                 };
-                Controller.prototype.structure1 = function () {
-                    var nodes = new Array();
-                    nodes.push(new CP.Mechanical.Node(1));
-                    nodes[0].position.x = 0;
-                    nodes[0].position.y = 0;
-                    nodes[0].displacement.x = 0;
-                    nodes[0].displacement.y = 0;
-                    nodes.push(new CP.Mechanical.Node(2));
-                    nodes[1].position.x = 40;
-                    nodes[1].position.y = 0;
-                    nodes[1].displacement.y = 0;
-                    nodes[1].force.x = 20000;
-                    nodes.push(new CP.Mechanical.Node(3));
-                    nodes[2].position.x = 40;
-                    nodes[2].position.y = 30;
-                    nodes[2].force.y = -25000;
-                    nodes.push(new CP.Mechanical.Node(4));
-                    nodes[3].position.x = 0;
-                    nodes[3].position.y = 30;
-                    nodes[3].displacement.x = 0;
-                    nodes[3].displacement.y = 0;
-                    var material = new CP.Mechanical.Material("Material 1", new CP.Mathematics.Value(29.5 * Math.pow(10, 6), "x10^6 psi"));
-                    var elements = [];
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[1]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[2], nodes[1]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[2]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[3], nodes[2]));
-                    var structure = new CP.Mechanical.TrussStructure(2, elements, nodes);
-                    structure.solve();
-                    return structure;
-                };
-                Controller.prototype.structure2 = function () {
-                    var nodes = new Array();
-                    nodes.push(new CP.Mechanical.Node(1));
-                    nodes[0].position.x = 0;
-                    nodes[0].position.y = 30;
-                    nodes[0].displacement.x = 0;
-                    nodes[0].displacement.y = 0;
-                    nodes.push(new CP.Mechanical.Node(2));
-                    nodes[1].position.x = 40;
-                    nodes[1].position.y = 30;
-                    nodes.push(new CP.Mechanical.Node(3));
-                    nodes[2].position.x = 40;
-                    nodes[2].position.y = 0;
-                    nodes[2].force.y = -10000;
-                    nodes.push(new CP.Mechanical.Node(4));
-                    nodes[3].position.x = 0;
-                    nodes[3].position.y = 0;
-                    nodes[3].displacement.x = 0;
-                    nodes[3].displacement.y = 0;
-                    nodes.push(new CP.Mechanical.Node(5));
-                    nodes[4].position.x = 80;
-                    nodes[4].position.y = 30;
-                    nodes[4].force.y = -25000;
-                    var material = new CP.Mechanical.Material("Material 1", new CP.Mathematics.Value(29.5 * Math.pow(10, 6), "x10^6 psi"));
-                    var elements = [];
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[1]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[2], nodes[1]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[2]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[3], nodes[2]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[1], nodes[4]));
-                    elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[2], nodes[4]));
-                    var structure = new CP.Mechanical.TrussStructure(2, elements, nodes);
-                    structure.solve();
-                    return structure;
-                };
-                Controller.prototype.getStructureDeinition = function () {
+                Controller.prototype.getStructureDefinition = function () {
                     return {
                         nodes: [
                             {
--- ts/apps/Truss/controller.ts
+++ ts/apps/Truss/controller.ts
@@ -17,7 +17,7 @@ module Sandbox.Apps.Truss {
                 }
             });
 
-            this.structureJson = JSON.stringify(this.getStructureDeinition(),null,"\t");
+            this.structureJson = JSON.stringify(this.getStructureDefinition(),null,"\t");
         }
 
         public loadStructure(structureDefinition: CP.Mechanical.StructureDefinition) {
@@ -26,88 +26,7 @@ module Sandbox.Apps.Truss {
             this.structure = structure;
         }
 
-        public structure1() {
-            var nodes = new Array<CP.Mechanical.Node>();
-            nodes.push(new CP.Mechanical.Node(1));
-            nodes[0].position.x = 0;
-            nodes[0].position.y = 0;
-            nodes[0].displacement.x = 0;
-            nodes[0].displacement.y = 0;
-
-            nodes.push(new CP.Mechanical.Node(2));
-            nodes[1].position.x = 40;
-            nodes[1].position.y = 0;
-            nodes[1].displacement.y = 0;
-            nodes[1].force.x = 20000;
-
-            nodes.push(new CP.Mechanical.Node(3));
-            nodes[2].position.x = 40;
-            nodes[2].position.y = 30;
-            nodes[2].force.y = -25000;
-
-            nodes.push(new CP.Mechanical.Node(4));
-            nodes[3].position.x = 0;
-            nodes[3].position.y = 30;
-            nodes[3].displacement.x = 0;
-            nodes[3].displacement.y = 0;
-
-            var material = new CP.Mechanical.Material("Material 1", new CP.Mathematics.Value(29.5 * Math.pow(10, 6), "x10^6 psi"));
-
-            var elements = [];
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[1]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[2], nodes[1]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[2]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[3], nodes[2]));
-            
-            var structure = new CP.Mechanical.TrussStructure(2, elements, nodes);
-            structure.solve();
-            return structure;
-        }
-
-        public structure2() {
-            var nodes = new Array<CP.Mechanical.Node>();
-            nodes.push(new CP.Mechanical.Node(1));
-            nodes[0].position.x = 0;
-            nodes[0].position.y = 30;
-            nodes[0].displacement.x = 0;
-            nodes[0].displacement.y = 0;
-
-            nodes.push(new CP.Mechanical.Node(2));
-            nodes[1].position.x = 40;
-            nodes[1].position.y = 30;
-
-            nodes.push(new CP.Mechanical.Node(3));
-            nodes[2].position.x = 40;
-            nodes[2].position.y = 0;
-            nodes[2].force.y = -10000;
-
-            nodes.push(new CP.Mechanical.Node(4));
-            nodes[3].position.x = 0;
-            nodes[3].position.y = 0;
-            nodes[3].displacement.x = 0;
-            nodes[3].displacement.y = 0;
-
-            nodes.push(new CP.Mechanical.Node(5));
-            nodes[4].position.x = 80;
-            nodes[4].position.y = 30;
-            nodes[4].force.y = -25000;
-
-            var material = new CP.Mechanical.Material("Material 1", new CP.Mathematics.Value(29.5 * Math.pow(10, 6), "x10^6 psi"));
-
-            var elements = [];
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[1]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[2], nodes[1]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[0], nodes[2]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[3], nodes[2]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[1], nodes[4]));
-            elements.push(new CP.Mechanical.TrussElement(material, new CP.Mathematics.Value(1), nodes[2], nodes[4]));
-
-            var structure = new CP.Mechanical.TrussStructure(2, elements, nodes);
-            structure.solve();
-            return structure;
-        }
-
-        public getStructureDeinition(): CP.Mechanical.StructureDefinition {
+        public getStructureDefinition(): CP.Mechanical.StructureDefinition {
             return {
                 nodes: [
                     {
